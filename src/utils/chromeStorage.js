2ab6ee4f31faa0c8484d7c977f227aa1bdac9d1d
ShetePro <sheteprolin@gmail.com>
2024-10-26 15:28:19 +0800

    feat(utils): update watchChromeStore function

--- src/utils/chromeStorage.js
+++ src/utils/chromeStorage.js
@@ -1,13 +1,12 @@
-export const storageKey = 'CopyToMarkdown'
+export const storageKey = "CopyToMarkdown";
 export const defaultStorage = {
   selectionPopup: true,
   contextMenus: true,
-  shortcut: '',
+  shortcut: "",
   popupOpacity: 1,
-}
+};
 export function setChromeStorage(key = storageKey, value) {
-  chrome?.storage?.local.set({ [key]: value }).then(() => {
-  });
+  chrome?.storage?.local.set({ [key]: value }).then(() => {});
 }
 
 export function getChromeStorage(key = storageKey) {
@@ -15,5 +14,14 @@ export function getChromeStorage(key = storageKey) {
     chrome?.storage?.local.get([key]).then((result) => {
       resolve(result[key] || defaultStorage);
     });
+  });
+}
+
+export function watchChromeStorage(callback, options = {}) {
+  const { type = "local", key = storageKey } = options;
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName === type && changes[storageKey]) {
+      callback(changes[storageKey]);
+    }
   });
 }
